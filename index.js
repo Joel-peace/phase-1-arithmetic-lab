46a61a865c8562794e3ebf9052c72c40a21f297c
Joel <joel.thayu@student.moringaschool.com>
2025-03-08 13:34:33 +0300

    Arthimetic

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// Create variables and set them to the required values
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // Result will be 62
+
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // The remainder will be 4
+
+let max = Math.max(1, 5, 10, 15, 20); // Returns the highest number (20)
+
+// Logging the results to verify
+console.log(multiply); // 62
+console.log(random); // Random integer greater than 0
+console.log(mod); // 4
+console.log(max); // 20
 
 // Write your code here
